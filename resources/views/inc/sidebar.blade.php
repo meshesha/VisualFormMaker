84043456795365a7073dd1b91eabdfe0fd7574da
meshesha <meshesha1@gmail.com>
2020-07-26 02:10:58 +0300

    update

--- resources/views/inc/sidebar.blade.php
+++ resources/views/inc/sidebar.blade.php
@@ -3,10 +3,8 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
 <!-- Brand Logo -->
 <a href="{{ url('/') }}" class="brand-link">
-    <!--
-    <img src="dist/img/Logo.png" alt=" Logo" class="brand-image img-circle elevation-3"
+    <img src="{{asset('storage/images')}}/logo-icon.png" alt=" Logo" class="brand-image elevation-3"
         style="opacity: .8">
-    -->
     <span class="brand-text font-weight-light">{{ config('app.name', 'VisualFormMaker') }}</span>
 </a>
 
